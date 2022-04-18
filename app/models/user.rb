f757dad649301fcc6b212e4dd4fe22e62b08c5be
Meri <mgogi13@freeuni.edu.ge>
2022-04-18 16:31:40 +0400

    test: add spec for users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many: categories, foreign_key: 'author_id', dependent: :destroy
-  has_many: purchases, foreign_key: 'author_id', dependent: :destroy
+  has_many :categories, foreign_key: 'author_id', dependent: :destroy
+  has_many :purchases, foreign_key: 'author_id', dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 3, maximum: 100 }
 end
