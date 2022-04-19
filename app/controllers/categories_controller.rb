8941bf6af1bbcfdcab9134bd122dd582ca15b958
Meri <mgogi13@freeuni.edu.ge>
2022-04-19 14:29:53 +0400

    feat: add categories views and actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,37 @@
 class CategoriesController < ApplicationController
-  def index; end
+  def index
+    @categories = current_user.categories
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @user = @category.user
+  end
+
+  def create
+    @category = current_user.categories.new(category_params)
+    if @category.save
+      flash[:notice] = 'Category was created successfully.'
+      redirect_to categories_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    flash[:notice] = 'Category was deleted successfully.'
+    redirect_to categories_path
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,6 +1,7 @@
 class Purchase < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id', class_name: 'User'
-  has_many :categories, dependent: :destroy
+  belongs_to :category, foreign_key: 'category_id'
+  has_many :categories, foreign_key: 'category_id', dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 3, maximum: 100 }
   validates :amount, presence: true, comparison: { greater_than: 0 }
