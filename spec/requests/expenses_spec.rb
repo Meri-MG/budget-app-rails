c9ae41d879426c2ff5b69eb63584d6b92baf769d
Meri <mgogi13@freeuni.edu.ge>
2022-04-22 12:56:09 +0400

    test: specs for expenses controller

--- spec/requests/expenses_spec.rb
+++ spec/requests/expenses_spec.rb
@@ -1,35 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe "Categories", type: :request do
-  describe "categories" do
-    it "GET /categories" do
-      get categories_path
+RSpec.describe "Expenses", type: :request do
+  describe "expenses" do
+    it "GET /expenses" do
+      get expenses_path
       expect(response).to render_template('index')
       expect(response).to have_http_status(200)
     end
 
-    it 'GET /categories/:id' do
+    it 'GET /expenses/:id' do
       user = User.create!(name: 'Barbare', email: 'barbare@test.come', password: 'karate', confirmed_at: Time.now)
-      category = user.categories.create!(name: 'Sports', icon: 'icon')
+      expense = user.expenses.create!(name: 'ball', amount: 3)
       sign_in user
-      get("/categories/#{category.id}")
+      get("/expenses/#{expense.id}")
       expect(response).to render_template('show')
       expect(response).to have_http_status(:ok)
     end
 
-    it 'GET /categories/new' do
-      get('/categories/new')
-      expect(response).to render_template('new')
-      expect(response).to have_http_status(:ok)
-    end
-
-    it 'DELETE /foods/:id' do
+    it 'DELETE /expenses/:id' do
       user = User.create!(name: 'Barbare', email: 'barbare@test.come', password: 'karate', confirmed_at: Time.now)
-      category = user.categories.create!(name: 'Sports', icon: 'icon')
+      expense = user.expenses.create!(name: 'ball', amount: 3)
       sign_in user
-      delete("/categories/#{category.id}")
+      delete("/expenses/#{expense.id}")
       expect do
-        get("/categories/#{category.id}")
+        get("/expenses/#{expense.id}")
       end.to raise_error(ActiveRecord::RecordNotFound)
       expect(response).to_not render_template('show')
       expect(response).to_not have_http_status(:ok)
